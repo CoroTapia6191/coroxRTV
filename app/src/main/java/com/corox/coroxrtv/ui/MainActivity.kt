a93d8fca38568795f1b00395b9440bc6f516d0ec
coro_user <theCoriche@gmail.com>
2023-11-10 00:35:28 -0500

    mvvm vew models and injections

--- app/src/main/java/com/corox/coroxrtv/ui/MainActivity.kt
+++ app/src/main/java/com/corox/coroxrtv/ui/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.corox.coroxrtv
+package com.corox.coroxrtv.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,8 +13,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.corox.coroxrtv.navigation.AppNavigation
 import com.corox.coroxrtv.ui.theme.CoroxRTVTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AppNavigation()
+                    AppNavigation(viewModel)
                 }
             }
         }
